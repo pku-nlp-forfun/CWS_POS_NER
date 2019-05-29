2cb1b56261be6e42fe855215c9ac41bacfcebd39
gunjianpan <iofu728@163.com>
2019-05-30 01:59:38 +0800

    🏌finish crf model

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from dataset import processing_pos_data, get_raw_article_from_cws_data
 from constant import POS_DATA, CWS_DATA, RESULT
 from evaluate import total_evaluate
-from model import POSModel
+from model import CWSModel, POSModel
 from POS.pos_data import load_cws_result_as_input, from_pos_list_to_file, load_cws_result_from_pos_as_input
 import os
 
@@ -20,7 +20,12 @@ def pos_test_by_cws_gold(pos_dict):
 def main():
     print('Data preprocessing for CWS and POS tagging...')
     cws_train, pos_dict = processing_pos_data(POS_DATA['Train'])
+    cws_dev, _ = processing_pos_data(POS_DATA['Dev'])
     cws_test, _ = processing_pos_data(POS_DATA['Test'])
+    print('Data over...')
+    cws = CWSModel(cws_train, cws_dev, cws_test)
+    print('Over')
+    cws.run_model()
 
     # Train
     print('Training CWS model...')
