c0c093059ed6ed86dd28e9f3172b3b3a37463791
gunjianpan <iofu728@163.com>
2019-05-31 00:45:44 +0800

    🦉fix seq end bug

--- CWS/cws_crf.py
+++ CWS/cws_crf.py
@@ -2,7 +2,7 @@
 # @Author: gunjianpan
 # @Date:   2019-05-28 22:24:44
 # @Last Modified by:   gunjianpan
-# @Last Modified time: 2019-05-30 19:43:28
+# @Last Modified time: 2019-05-31 00:43:07
 
 import constant as con
 import numpy as np
@@ -34,13 +34,19 @@ def mask(num_word: int, seq_len: List):
     return mask_matrix, total_labels
 
 
-def evaluation(y: List, y_predict: List, mask, total_label, types: str):
+def evaluation(y: List, y_predict: List, seq:List, mask, total_label, types: str):
     # print(y.shape, y_predict.shape, mask.shape)
     y = sum([list(jj[:sum(mask[ii])]) for ii, jj in enumerate(y)], [])
     y = [int(ii > 1) for ii in y]
     y_predict = sum([list(jj[:sum(mask[ii])])
                      for ii, jj in enumerate(y_predict)], [])
     y_predict = [int(ii > 1) for ii in y_predict]
+    change_idx, idx = [], -1
+    for ii in seq:
+        change_idx.append(ii + idx)
+        idx += ii
+    for ii in change_idx:
+        y_predict[ii] = 1
 
     # correct_labels = np.sum((y == y_predict) * mask)
     # accuracy = 100.0 * correct_labels / float(total_label)
@@ -55,7 +61,7 @@ def fastF1(result, predict):
     ''' cws f1 score calculate '''
     recallNum = sum(result)
     precisionNum = sum(predict)
-    last_result, last_predict, trueNum = 0, 0, 0
+    last_result, last_predict, trueNum = -1, -1, 0
     for ii in range(len(result)):
         if result[ii] and result[ii] == predict[ii] and last_predict == last_result:
             trueNum += 1
@@ -63,7 +69,7 @@ def fastF1(result, predict):
             last_result = ii
         if predict[ii]:
             last_predict = ii
-            
+    # print(trueNum, precisionNum, recallNum)
     r = trueNum / recallNum if recallNum else 0
     p = trueNum / precisionNum if precisionNum else 0
     macro_f1 = (2 * p * r) / (p + r) if (p + r) else 0
@@ -71,9 +77,9 @@ def fastF1(result, predict):
     return p * 100, r * 100, macro_f1 * 100
 
 
-def crf_tf(train_x: List, train_y: List, train_seq: List,
-           dev_x: List, dev_y: List, dev_seq: List,
-           test_x: List, test_y: List, test_seq: List, num_tag: int):
+def crf_tf(train_x: List, train_y: List, train_seq: List, train_se: List,
+           dev_x: List, dev_y: List, dev_seq: List, dev_se: List,
+           test_x: List, test_y: List, test_seq: List, test_se: List, num_tag: int):
     ''' crf base on tensorflow '''
 
     with tf.Graph().as_default(), tf.Session() as session:
@@ -130,14 +136,14 @@ def crf_tf(train_x: List, train_y: List, train_seq: List,
             if i % 10 == 0:
                 dev_predict = session.run([dev_viterbi_seq])[0]
                 print(f'------  \033[92m{i} epochs \033[0m -------')
-                train_p, train_r, train_macro_f1 = evaluation(train_y, train_predict, train_mask, train_total, 'Train')
-                dev_p, dev_r, dev_macro_f1 = evaluation(dev_y, dev_predict, dev_mask, dev_total, 'Dev')
+                train_p, train_r, train_macro_f1 = evaluation(train_y, train_predict, train_se, train_mask, train_total, 'Train')
+                dev_p, dev_r, dev_macro_f1 = evaluation(dev_y, dev_predict, dev_se, dev_mask, dev_total, 'Dev')
                 log(f'{i}|{train_p:.2f}|{train_r:.2f}|{train_macro_f1:.2f}|{dev_p:.2f}|{dev_r:.2f}|{dev_macro_f1:.2f}|')
                 if dev_macro_f1 > best_dev_acc:
                     best_dev_acc = dev_macro_f1
                     test_predict = session.run([test_viterbi_seq])[0]
                     pickle.dump(test_predict, open(f"{con.RESULT['CWS']}.pkl", 'wb'))
-                    test_p, test_r, test_macro_f1 = evaluation(test_y, test_predict, test_mask, test_total, 'Test')
+                    test_p, test_r, test_macro_f1 = evaluation(test_y, test_predict, test_se, test_mask, test_total, 'Test')
                         
         log(f"Best Dev Macro_f1: {best_dev_acc:.2f}%")
         log(f"Best Test P: {test_p:.2f}%, R: {test_r:.2f}%, Macro_f1: {test_macro_f1:.2f}%")
@@ -150,8 +156,9 @@ def crf_tf(train_x: List, train_y: List, train_seq: List,
 def test_params(num_seq: int, num_word: int, num_fea: int, num_tag: int):
     x = np.random.rand(num_seq, num_word, num_fea).astype(np.float32)
     y = np.random.randint(num_tag, size=[num_seq, num_word]).astype(np.int32)
-    seq = np.random.randint(max(num_word - 5, 10), num_word, size=[num_seq]).astype(np.int32)
-    return x, y, seq
+    seq_len = np.random.randint(max(num_word - 5, 10), num_word, size=[num_seq]).astype(np.int32)
+    seq = np.random.randint(0, 5, size=[num_seq]).astype(np.int32)
+    return x, y, seq_len, seq
 
 
 if __name__ == "__main__":
@@ -159,8 +166,8 @@ if __name__ == "__main__":
     num_word = 20
     num_fea = 100
     num_tag = 4
-    train_x, train_y, train_seq = test_params(num_seq, num_word, num_fea, num_tag)
-    dev_x, dev_y, dev_seq = test_params(10, num_word, num_fea, num_tag)
-    test_x, test_y, test_seq = test_params(10, 30, num_fea, num_tag)
+    train_x, train_y, train_seq, train_se = test_params(num_seq, num_word, num_fea, num_tag)
+    dev_x, dev_y, dev_seq, dev_se = test_params(10, num_word, num_fea, num_tag)
+    test_x, test_y, test_seq, test_se = test_params(10, 30, num_fea, num_tag)
 
-    crf_tf(train_x, train_y, train_seq, dev_x, dev_y, dev_seq, test_x, test_y, test_seq, num_tag)
+    crf_tf(train_x, train_y, train_seq, train_se, dev_x, dev_y, dev_seq, dev_se, test_x, test_y, test_seq, test_se, num_tag)
